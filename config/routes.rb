30e582dc600c646a28fbab64b0ccd19794bf7d84
Jose <jrmolanor@unal.edu.co>
2017-04-07 10:06:18 -0500

    Rutas 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :clients, :employees, :stores, :appointments
+ 
+  resources :admins do
+  	resources :stores
+  end
+
+  namespace :clients do
+  	resources :purchases, :appointments 
+  end
+
+  resources :employee do
+  	resources :appointments;
+  end
+  #Rutas de consulta límitadas a :list y :show 
+  namespace :stores do
+  	resources :products, only: [:list, :show] do
+  		resources :images, :categories, only: [:list, :show]
+  	end
+  	resources :employees do
+  		resources :appointments
+  	end
+  end
+
+  resources :categories do
+  	resources :products, only: [:list, :show]
+  end
+
 end
