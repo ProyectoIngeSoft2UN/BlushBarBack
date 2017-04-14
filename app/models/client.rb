b388cc3a82766ffcee5952994230a1b97efa59fe
Sergio Castro <sacastroc@unal.edu.co>
2017-04-13 23:43:43 -0500

    Añadido gemas carrierwave y fog

--- app/models/client.rb
+++ app/models/client.rb
@@ -11,8 +11,8 @@ class Client < ApplicationRecord
 	validates :lastName,  presence: true, format: { with: /[a-z ,.'-]+/i, message: "LastName must be string" }
 	validates :email, presence: true, format: { with: /[\+A-Z0-9\._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}/i, message: "person@example.com" }
 	validates :phone, presence: true, allow_blank: true
-	validates :address
-	validates :city
+	validates :address, presence: true
+	validates :city, presence: true
 	#validates :phone, presence: true,  format: { with:  /[0-9]*/, message: "Phone must be a number" }
 
 	def self.get_clients(page, per_page)
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :bills
   has_and_belongs_to_many :images
 
+  mount_uploader :images, ImageUploader
   validates :name, presence: {message: 'Nombre no debe ser vacio'}
 	validates :description, presence: {message: 'Descripcion no debe ser vacia'}
 	validates :cost, numericality: true, presence: {message: 'El cosot no debe ser vacio'}
